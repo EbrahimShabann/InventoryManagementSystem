30d092d0a06ac7a1a09c298c0b2f307dfb70b7e5
EbrahimShabann <ibrahimshaban247@gmail.com>
2025-07-05 15:16:51 +0300

    update home

--- InventoryManagementSystem/Controllers/HomeController.cs
+++ InventoryManagementSystem/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repositories.IRepositories;
+using InventoryManagementSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryManagementSystem.Controllers
@@ -7,15 +9,28 @@ namespace InventoryManagementSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork uof;
+        private readonly IproductRepo productRepo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork uof,IproductRepo productRepo)
         {
             _logger = logger;
+            this.uof = uof;
+            this.productRepo = productRepo;
         }
+        
 
         public IActionResult Index()
         {
-            return View();
+            var homeVM = new homeViewModel
+            {
+                warehousesNum = uof.warehouseRepo.GetAll().Count,
+                categoriesNum = uof.categoryRepo.GetAll().Count,
+                productsNum = productRepo.GetAll().Count,
+                suppliersNum = uof.supplierRepo.GetAll().Count,
+                LowStockProducts=productRepo.GetAll().Where(p=>p.TotalStocQuantity<=5),
+            }; 
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ InventoryManagementSystem/ViewModels/homeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repositories.IRepositories;
+
+namespace InventoryManagementSystem.ViewModels
+{
+    public class homeViewModel
+    {
+      
+        public int warehousesNum { get; set; }
+        public int categoriesNum { get; set; }
+        public int productsNum { get; set; }
+        public int suppliersNum { get; set; }
+        public IEnumerable<Product> LowStockProducts { get; set; }
+    }
+}
